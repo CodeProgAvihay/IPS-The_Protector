8fd8b10abd5bb987c01a9975866783bc456d4699
Avihay_Programmer <avihai6025@gmail.com>
2025-11-09 23:02:09 +0200

    Finished the main file.

--- main.py
+++ main.py
@@ -1,18 +1,50 @@
+import signal
+import sys
 import os
 from netfilterqueue import NetfilterQueue
 from scapy.all import Ether
 
+def require_root():
+    if os.geteuid() != 0:
+        print("[X] You must run the program with root permissions(sudo).")
+        sys.exit(1)
+
+def cleanup_and_exit(nfqueue, signum=None, frame=None):
+    try:
+        nfqueue.unbind()
+    except Exception as e:
+        pass
+    os.system("iptables -F")
+    sys.exit(0)
+
 def process_packet(packet):
-    pass
+    try:
+        scapy_pck = Ether(packet.get_payload())
+        #here we connect it to the checking system.
+        #if is_attack:
+        #   packet.drop()
+        #else:
+        #   packet.accept()
+    except Exception as e:
+        print("Error analyze the packet.")
+        packet.accpet()
 
 def main():
+    require_root()
     os.system("iptables -I INPUT -j NFQUEUE --queue-num 1")
     nfqueue = NetfilterQueue()
+
+    #checking when the program closed:
+    signal.signal(signal.SIGINT, lambda s, f: cleanup_and_exit(nfqueue, s, f)) #by Ctrl + C
+    signal.signal(signal.SIGTERM, lambda s, f: cleanup_and_exit(nfqueue, s, f)) #by killing the terminal.
+    signal.signal(signal.SIGHUP, lambda s, f: cleanup_and_exit(nfqueue, s, f)) #by disconnecting from the terminal.
+
     nfqueue.bind(1, process_packet)
+    print("Starting listennig to packets...")
     try:
         nfqueue.run()
     except KeyboardInterrupt:
-        os.system("iptables -F")
+       cleanup_and_exit(nfqueue)
 
 
 if __name__ == "__main__":
