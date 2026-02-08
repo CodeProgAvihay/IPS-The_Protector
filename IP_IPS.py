149b2943e2fc308a09bf7e64c48f4c29b0586ee6
Avihay_Programmer <avihai6025@gmail.com>
2026-02-08 19:06:51 +0200

    Changed the IP_IPS file that it will make the DB work with the Threads.

--- IP_IPS.py
+++ IP_IPS.py
@@ -12,14 +12,6 @@ import system_status
 
 IP = scapy.IP
 
-DB = SqliteDatabase()
-
-HANDLERS = [
-    PortScanningHandler(DB),
-    SynFloodHandler(DB),
-    DnsSpoofingHandler(DB)
-]
-
 blocked_ip = set()
 
 
@@ -38,40 +30,47 @@ def cleanup_and_exit(nfqueue, signum=None, frame=None):
     # sys.exit(0)
 
 
-def process_packet(packet):
-    try:
-        scapy_pck = IP(packet.get_payload())
-        if scapy_pck[IP].src == "192.168.1.15":
-            print("[*] Packet of check.")
-        if scapy_pck[IP].src in blocked_ip:
-            packet.drop()
-            print("{&} Dropped the attack before checking.")
-            return
-        for handler in HANDLERS:
-            if handler.detect(scapy_pck):
+def clousre_to_process(HANDLERS):
+    def process_packet(packet):
+        try:
+            scapy_pck = IP(packet.get_payload())
+            if scapy_pck[IP].src == "192.168.1.15":
+                print("[*] Packet of check.")
+            if scapy_pck[IP].src in blocked_ip:
                 packet.drop()
-                blocked_ip.add(scapy_pck[IP].src)
-                print("Dropped the attack.")
+                print("{&} Dropped the attack before checking.")
                 return
-        packet.accept()
-        print("packet got accepted")
-    except Exception as e:
-        print("Error analyze the packet.")
-        print("Error:", e)
-        packet.accept()
+            for handler in HANDLERS:
+                if handler.detect(scapy_pck):
+                    packet.drop()
+                    blocked_ip.add(scapy_pck[IP].src)
+                    print("Dropped the attack.")
+                    return
+            packet.accept()
+            print("packet got accepted")
+        except Exception as e:
+            print("Error analyze the packet.")
+            print("Error:", e)
+            packet.accept()
+    return process_packet
 
 
 def main():
     require_root()
     os.system("iptables -I INPUT -j NFQUEUE --queue-num 1")
     nfqueue = NetfilterQueue()
-
+    DB = SqliteDatabase()
+    HANDLERS = [
+    PortScanningHandler(DB),
+    SynFloodHandler(DB),
+    DnsSpoofingHandler(DB)
+    ]
     # checking when the program closed:
     # signal.signal(signal.SIGINT, lambda s, f: cleanup_and_exit(nfqueue, s, f)) #by Ctrl + C
     # signal.signal(signal.SIGTERM, lambda s, f: cleanup_and_exit(nfqueue, s, f)) #by killing the terminal.
     # signal.signal(signal.SIGHUP, lambda s, f: cleanup_and_exit(nfqueue, s, f)) #by disconnecting from the terminal.
-
-    nfqueue.bind(1, process_packet)
+    process_func = clousre_to_process(HANDLERS)
+    nfqueue.bind(1, process_func)
     print("Starting listennig to packets...")
     try:
         while system_status.running:
